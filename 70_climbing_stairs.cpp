6df8f8d81fd2b6cb931a581e70341ce9dabc7750
Balakrishna Reddy <balakrishnareddy@Disconnected.local>
2023-03-12 00:56:25 +0530

    climbing stairs
     solution for climbing stairs problem is added

---
+++ 70_climbing_stairs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int64_t arr[] = {0,1}, temp=0;
+        for (int i=0; i<n; i++){
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = arr[1]+temp;
+        }
+        return arr[1];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.climbStairs(3) << endl;
+}
